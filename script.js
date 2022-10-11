3a0ef48fbf43cb7a55ca6d8aba5555336192cd05
Kamil <kamilbialek2k05@gmail.com>
2022-10-11 17:31:33 +0200

    add new function that plays one round of the game and change first letter of chocies in getComputerChoice :

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function getComputerChoice() {
-const gameTools = ["Rock","Paper","Scissors"]
+const gameTools = ["rock","paper","scissors"]
 var computerChoice = gameTools[Math.floor(Math.random()*gameTools.length)];
 console.log(computerChoice)
 }
+function playRound(playerSelection, computerSelection) {
+   
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
