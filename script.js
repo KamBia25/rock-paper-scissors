25138c4e103769361ba48aad0a9cbfc496f02130
Kamil <kamilbialek2k05@gmail.com>
2022-10-11 18:04:15 +0200

    edit second function

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function getComputerChoice() {
-const gameTools = ["rock","paper","scissors"]
+const gameTools = ["rock","paper","scissors"];
 var computerChoice = gameTools[Math.floor(Math.random()*gameTools.length)];
-console.log(computerChoice)
+console.log(computerChoice);
 }
 function playRound(playerSelection, computerSelection) {
-   
+   let playerChoice = prompt("Write which one you want to play[rock, paper, scissors]:");
+   computerSelection = computerChoice;
+   if (computerSelection.toLowerCase() == playerSelection.toLowerCase() )
+        console.log("Draw, your choice was the same as the AI's")
+    
   }
-   
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
