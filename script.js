ab2ebb6f7b3dde8cd887187e7b74c69b899d56c5
Kamil <kamilbialek2k05@gmail.com>
2022-10-13 18:37:28 +0200

    add to function conditions for win and lose

--- script.js
+++ script.js
@@ -9,5 +9,28 @@ function playRound(playerSelection, computerSelection) {
    computerSelection = computerChoice;
    console.log(computerSelection)
    console.log(playerSelection)
-   
+   if (playerSelection == computerSelection){
+      console.log("draw")
+   }
+   if (playerSelection =! computerSelection)
+   {
+      if(playerSelection.toLowerCase() == "rock"|| computerSelection == "paper"){
+         console.log("defeat")
+      }
+      if(playerSelection.toLowerCase() == "rock"|| computerSelection == "scissors"){
+         console.log("victory")
+      }
+      if(playerSelection.toLowerCase() == "paper"|| computerSelection == "scissors"){
+         console.log("defeat")
+      }
+      if(playerSelection.toLowerCase() == "paper"|| computerSelection == "rock"){
+         console.log("victory")
+      }
+      if(playerSelection.toLowerCase() == "scissors"|| computerSelection == "rock"){
+         console.log("defeat")
+      }
+      if(playerSelection.toLowerCase() == "scissors"|| computerSelection == "paper"){
+         console.log("victory")
+      }
+   }
   }
